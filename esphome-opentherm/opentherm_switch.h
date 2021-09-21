6d5a6b78f445dfb221ec459d9cb3fd5e7b154142
Wanno Drijfhout <github.com@wanno.eu>
2021-09-21 22:29:14 +0000

    Fix switch

--- esphome-opentherm/opentherm_switch.h
+++ esphome-opentherm/opentherm_switch.h
@@ -3,13 +3,19 @@
 #include "esphome.h"
 
 class OpenthermSwitch : public Switch {
- public:
-  void write_state(bool state) override {
-    // This will be called every time the user requests a state change.
+  private:
+    bool state_;
+  public:
+    explicit OpenthermSwitch(bool state) : state_(state) {}
+    
+    float get_state() const { return state_; }
+  protected:
+    void write_state(bool state) override {
+      // This will be called every time the user requests a state change.
+      ESP_LOGD("opentherm_switch", "write_state");
+      state_ = state;
 
-    ESP_LOGD("opentherm_switch", "write_state");
-
-    // Acknowledge new state by publishing it
-    publish_state(state);
-  }
+      // Acknowledge new state by publishing it
+      publish_state(state);
+    }
 };
